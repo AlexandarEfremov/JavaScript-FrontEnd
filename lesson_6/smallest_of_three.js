828c639cda034d474ecf8c55c05ae88bd8ad83ba
Alex <efremov.alex@yahoo.bg>
2025-03-26 07:20:15 +0200

    smallest_of_three.js

---
+++ lesson_6/smallest_of_three.js
@@ -0,0 +1,8 @@
+function smallestNum(...nums) {
+    return Math.min(...nums);
+}
+
+console.log(smallestNum(2, 5, 3))
+console.log(smallestNum(600, 342, 123))
+console.log(smallestNum(25, 21, 4))
+console.log(smallestNum(2, 2, 2))
