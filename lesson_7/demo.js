101ee8d45df50cc6eb178c1db31996e2d05261f5
Alex <efremov.alex@yahoo.bg>
2025-03-28 07:22:58 +0200

    person_info.js

---
+++ lesson_7/demo.js
@@ -0,0 +1,6 @@
+const myObj = { name: 'Peter', age: 21 };
+
+myObj.height = 183;
+
+delete myObj.age;
+console.log(myObj);
---
+++ lesson_7/person_info.js
@@ -0,0 +1,7 @@
+function solve(fName, lName, age) {
+    let personInfo = { firstName: `${fName}`, lastName: `${lName}`, age: `${age}` };
+    // return console.log(`firstName: ${personInfo.firstName}\nlastName: ${personInfo.lastName}\nage ${personInfo.age}`);
+    return personInfo;
+}
+
+solve("Peter", "Pan", "20")
