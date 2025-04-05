22ef4f76295343816077fd109c7167fc64f1e713
Alex <efremov.alex@yahoo.bg>
2025-04-05 13:54:09 +0300

    all

--- lesson_10/01. Subtraction/subtraction.js
+++ lesson_10/01. Subtraction/subtraction.js
@@ -1,3 +1,13 @@
 function subtract() {
-    //TODO
+    let numOne = document.getElementById("firstNumber");
+    let numTwo = document.getElementById("secondNumber");
+
+    numOne = Number(numOne.value);
+    numTwo = Number(numTwo.value);
+
+    let subtract = numOne - numTwo;
+    let output = document.getElementById("result");
+
+
+    output.textContent += subtract;
 }
