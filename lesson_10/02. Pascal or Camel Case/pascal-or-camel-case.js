7af807b7a53514906f25a48cfb36e09ad5122b78
Alex <efremov.alex@yahoo.bg>
2025-04-05 16:01:58 +0300

    all

--- lesson_10/02. Pascal or Camel Case/pascal-or-camel-case.js
+++ lesson_10/02. Pascal or Camel Case/pascal-or-camel-case.js
@@ -1,3 +1,29 @@
 function solve() {
-  //TODO
+  let text = document.getElementById("text").value;
+  let resultToLower = text.toLowerCase().split(' ');
+  let tpyingCase = document.getElementById("naming-convention").value;
+  let output = '';
+
+  if (tpyingCase == 'Camel Case') {
+    counter = 0;
+    for (let word of resultToLower) {
+      if (counter > 0) {
+        word = word.replace(word[0], word[0].toUpperCase());
+        output += word;
+      } else {
+        output += word;
+      }
+      counter++
+    }
+  } else if (tpyingCase == 'Pascal Case') {
+    for (let word of resultToLower) {
+      word = word.replace(word[0], word[0].toUpperCase());
+      output += word;
+    }
+  } else {
+    output = 'Error!'
+  }
+
+  let result = document.getElementById("result")
+  result.textContent = output;
 }
