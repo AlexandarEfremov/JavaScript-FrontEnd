147cc02f8f915c9109b3c6797bc6b9d04668c88a
Alex <efremov.alex@yahoo.bg>
2025-04-06 18:10:44 +0300

    all

--- lesson_10/07. Hell's Kitchen/hells-kitchen.js
+++ lesson_10/07. Hell's Kitchen/hells-kitchen.js
@@ -1,3 +1,22 @@
 function solve() {
-    //TODO
+    let textArea = document.getElementsByTagName('textarea')[0];
+    let entries = textArea.textContent;
+    let jsonEntries = JSON.parse(entries);
+
+    let myObj = {};
+
+    for (let entries of jsonEntries) {
+        let [restaurant, workers] = entries.split(' - ');
+        workers = workers.split(', ');
+        for (let i of workers) {
+            let [worker, salary] = i.split(' ');
+            myObj[restaurant] = {
+                [worker]: salary,
+            }
+        }
+    }
+
+    for (let entry of Object.entries(myObj)) {
+        console.log(entry);
+    }
 }
